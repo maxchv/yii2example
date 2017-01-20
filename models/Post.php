73dd37028893eec8ed383483886a88201591714d
Maxim <maxshaptala@gmail.com>
2017-01-20 19:37:41 +0200

    Сгенерирована модель Сообщения через gii

---
+++ models/Post.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property integer $category_id
+ * @property integer $status
+ * @property string $created
+ * @property string $updated
+ * @property integer $user_id
+ *
+ * @property Comment[] $comments
+ * @property Category $category
+ * @property User $user
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'category_id', 'user_id'], 'required'],
+            [['content'], 'string'],
+            [['category_id', 'status', 'user_id'], 'integer'],
+            [['created', 'updated'], 'safe'],
+            [['title'], 'string', 'max' => 128],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'category_id' => 'Category ID',
+            'status' => 'Status',
+            'created' => 'Created',
+            'updated' => 'Updated',
+            'user_id' => 'User ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['post_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
