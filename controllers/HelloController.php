54af7b10e3e16da75419a67ca448f2aa90dbcf6d
Maxim <maxshaptala@gmail.com>
2017-01-20 17:11:02 +0200

    Добавлен контроллер hello с действием greeting

---
+++ controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaptala
+ * Date: 20.01.2017
+ * Time: 17:09
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class HelloController extends Controller
+{
+    public function actionGreeting() {
+        return "Hello World";
+    }
+}
