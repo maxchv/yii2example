b810cf549bdd83bc754cc38aab59cccea21625b6
Maxim <maxshaptala@gmail.com>
2017-01-20 19:40:27 +0200

    Сгенерирована модель BlogUser через gii

---
+++ models/BlogUser.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $login
+ * @property string $password
+ * @property string $email
+ * @property string $nickname
+ * @property string $about
+ *
+ * @property Post[] $posts
+ */
+class BlogUser extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'password', 'email', 'nickname'], 'required'],
+            [['about'], 'string'],
+            [['login'], 'string', 'max' => 40],
+            [['password', 'email'], 'string', 'max' => 100],
+            [['nickname'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'password' => 'Password',
+            'email' => 'Email',
+            'nickname' => 'Nickname',
+            'about' => 'About',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Post::className(), ['user_id' => 'id']);
+    }
+}
